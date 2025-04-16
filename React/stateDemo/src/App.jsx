580bb438dbd5b78ca97998ad4c07415cdde06f65
prasad-bigdp <bigdp585@gmail.com>
2025-04-16 10:50:27 +0530

    update

--- React/stateDemo/src/App.jsx
+++ React/stateDemo/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function App ()
+{
+  // let c = 0;
+  // function change ()
+  // {
+  //   c++;
+  //   console.log(c)
+  // }
+  const [c, setC] = useState(0)
+  const [color, setColor] = useState('#ffffff')
+  function changeColor (e)
+  {
+    setColor(e.target.value)
+    console.log("hello world")
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='myDiv' style={{backgroundColor:color}}>
+      <input type="color" placeholder='Enter a color' onChange={changeColor}/>
+			<h1>{c}</h1>
+			<button onClick={() => setC((c) => c + 1)}>+</button>
+			<button onClick={() => setC((c) => c - 1)}>-</button>
+			<button onClick={() => setC(0)}>reset</button>
+		</div>
+	)
 }
 
 export default App
